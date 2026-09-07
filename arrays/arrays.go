6a7f51d08d23fd05e9e0ee4a8f116bd3f1886fc3
Sam Gonsalves <samueldgonsalves@gmail.com>
2026-09-06 23:46:04 -0500

    confused an array with a slice

--- arrays/arrays.go
+++ arrays/arrays.go
@@ -5,25 +5,25 @@ import "fmt"
 var nums [5]int
 
 func main() {
-	nums := []int{1, 2, 3, 4, 5}
+	nums := [5]int{1, 2, 3, 4, 5}
 	printArray(nums)
 
 	changeSign(nums)
 
 	printArray(nums)
 
-	primes := []int{2, 3, 5, 7}
+	primes := [5]int{2, 3, 5, 7}
 
 	printArray(primes)
 }
 
-func changeSign(nums []int) {
+func changeSign(nums [5]int) {
 	for i := range nums {
 		nums[i] = nums[i] * (-1)
 	}
 }
 
-func printArray(nums []int) {
+func printArray(nums [5]int) {
 	for i := range nums {
 		fmt.Print(nums[i], ",")
 	}
