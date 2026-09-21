9d79949e6a4b969eb4e2cc5f29f5d345d9d0047b
Sam Gonsalves <samueldgonsalves@gmail.com>
2026-09-21 11:32:38 -0500

    Variadic functions

--- functions/writingfunctions.go
+++ functions/writingfunctions.go
@@ -6,6 +6,7 @@ func main() {
 	fmt.Println(adder(2, 3))
 	fmt.Println(sameArgsAdder(2, 3))
 	fmt.Println(returnManyValues(2, 3))
+	fmt.Println(variableNumberOfArguments(1, 2, 3, 4, 5))
 }
 
 func adder(a int, b int) int {
@@ -20,4 +21,13 @@ func sameArgsAdder(a, b int) int {
 // Avoids complexities around wrapping objects just to return them in a single type
 func returnManyValues(a, b int) (int, int) {
 	return a + 1, b + 1
+}
+
+func variableNumberOfArguments(a ...int) int {
+	sum := 0
+	// Note: the _ is ignored index, num is the value
+	for _, num := range a {
+		sum += num
+	}
+	return sum
 }
